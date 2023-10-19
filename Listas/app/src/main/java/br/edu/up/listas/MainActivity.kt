6e8240499c518c3437d7b9a0e20c3ed5759ef703
Lucas Gabriel <ltesteteste138@gmail.com>
2023-10-18 23:50:15 -0300

    integração-Firebase

--- Listas/app/src/main/java/br/edu/up/listas/MainActivity.kt
+++ Listas/app/src/main/java/br/edu/up/listas/MainActivity.kt
@@ -17,47 +17,67 @@ import android.widget.Toast
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var customAdapter: CustomAdapter
     private var SEU_REQUEST_CODE = 1
-    private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var editor: SharedPreferences.Editor
     private var dataset = mutableListOf<Produto>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
+        val db = FirebaseFirestore.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val intent = Intent(this, activity_Temporaria::class.java)
         startActivity(intent)
 
-
         val recicle: RecyclerView = findViewById(R.id.reciclerView)
-        sharedPreferences = getSharedPreferences("MinhasPreferencias", Context.MODE_PRIVATE)
-        editor = sharedPreferences.edit()
-        //pego o ultimo id
-        var contadorProdutos = sharedPreferences.getInt("contador_produtos", 0)
-        Log.d("MainActivity", "produtos cadastrados: $contadorProdutos")
 
-;       //preencho a list dataset com os produtos
-        for (i in 1..contadorProdutos) {
-            val idProduto = sharedPreferences.getString("produto_${i}_id", "")?:""
-            val nomeProduto = sharedPreferences.getString("produto_${i}_nome", "")?:""
-            val quantidadeProduto = sharedPreferences.getInt("produto_${i}_quantidade", 0)
-            val marcado = sharedPreferences.getBoolean("produto_${i}_marcado", false)
+        // Use uma Coroutine para buscar os produtos
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val querySnapshot = db.collection("Produtos")
+                    .get()
+                    .await()
 
-            if (nomeProduto.isNotEmpty() && quantidadeProduto > 0) {
-                val produto = Produto(idProduto, nomeProduto, quantidadeProduto, marcado)
-                dataset.add(produto)
+                val novosProdutos = mutableListOf<Produto>()
+                for (document in querySnapshot) {
+                    val nome = document.getString("nome") ?: "erro"
+                    val quantidade = document.getLong("qtd")?.toInt() ?: 0
+                    val marcado = document.getBoolean("marcado") ?: false
+
+                    val produto = Produto(nome, quantidade, marcado)
+                    novosProdutos.add(produto)
+                }
+                // Atualizar o dataset com os novos produtos na thread principal
+                withContext(Dispatchers.Main) {
+                    dataset.clear()
+                    dataset.addAll(novosProdutos)
+                    customAdapter.notifyDataSetChanged()
+                }
+            } catch (e: Exception) {
+                // Lidar com erros, como conexão com o Firestore
+                Log.e("MainActivity", "Erro ao buscar produtos: $e")
             }
         }
+
+
 
         customAdapter = CustomAdapter(dataset, this)
         recicle.layoutManager = LinearLayoutManager(this)
         recicle.adapter = customAdapter
 
-        val itemTouchHelper = ItemTouchHelper(object :ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
@@ -70,25 +90,33 @@ class MainActivity : AppCompatActivity() {
 
                 // Recuperar as informações do produto que está sendo movido/deslizado
                 val produtoRemovido = dataset[position]
-                val produtoId = produtoRemovido.id
+                val produtoNome = produtoRemovido.nome
 
                 // Remover o produto do dataset
                 dataset.removeAt(position)
                 customAdapter.notifyItemRemoved(position)
 
-                // Remova as informações do produto que está sendo movido/deslizado no sharedPreferences
-                if (produtoId != null) {
-                    editor.remove("produto_${produtoId}_id")
-                    editor.remove("produto_${produtoId}_nome")
-                    editor.remove("produto_${produtoId}_quantidade")
-                    editor.remove("produto_${produtoId}_marcado")
-                    editor.apply()
+                // Execute a exclusão em uma Coroutine
+                CoroutineScope(Dispatchers.IO).launch {
+                    // Código de exclusão no Firestore
+                    val db = FirebaseFirestore.getInstance()
+                    val produtosCollection = db.collection("Produtos")
+
+                    val querySnapshot = produtosCollection
+                        .whereEqualTo("nome", produtoNome)
+                        .get()
+                        .await()
+
+                    for (document in querySnapshot) {
+                        produtosCollection.document(document.id).delete()
+                            .addOnSuccessListener {
+                                Log.d("MainActivity", "Produto excluído do Firestore: $produtoNome")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w("MainActivity", "Erro ao excluir o produto do Firestore", e)
+                            }
+                    }
                 }
-                var contadorProdutos = sharedPreferences.getInt("contador_produtos", 0)
-
-                editor.putInt("contador_produtos", contadorProdutos)
-                editor.apply()
-
             }
         }
         )
@@ -96,12 +124,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     class CustomAdapter(private val dataSet: MutableList<Produto>, private val context: Context) :
-        RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val nomeTextView: TextView
             val qtdTextView: TextView
             val marcadocheckboxView: CheckBox
-
             init {
                 nomeTextView = view.findViewById(R.id.nomeTextView)
                 qtdTextView = view.findViewById(R.id.cursoTextView)
@@ -112,33 +139,53 @@ class MainActivity : AppCompatActivity() {
             parent: ViewGroup,
             viewType: Int
         ): CustomAdapter.ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.text_row_item, parent, false)
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.text_row_item, parent, false)
             return ViewHolder(view)
         }
+
         override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
             val produto = dataSet[position]
             holder.nomeTextView.text = produto.nome
             holder.qtdTextView.text = produto.qtd.toString()
 
             holder.marcadocheckboxView.isChecked = produto.marcado
-
             holder.marcadocheckboxView.setOnCheckedChangeListener { _, isChecked ->
                 // Atualize a propriedade "marcado" do produto quando o CheckBox for alterado
                 produto.marcado = isChecked
+                // Inicie uma coroutine para atualizar o produto no Firebase
+                CoroutineScope(Dispatchers.IO).launch {
+                    atualizarProdutoNoFirebase(produto)
+                }
+            }
+        }
 
-                // Agora, você pode salvar o estado marcado no SharedPreferences usando o contexto
-                val sharedPreferences = context.getSharedPreferences("MinhasPreferencias", Context.MODE_PRIVATE)
-                val editor = sharedPreferences.edit()
-                editor.putBoolean("produto_${produto.id}_marcado", isChecked) // Use o ID do produto para identificar a entrada no SharedPreferences
-                editor.apply()
+        private suspend fun atualizarProdutoNoFirebase(produto: Produto) {
+            val db = FirebaseFirestore.getInstance()
+            val produtosCollection = db.collection("Produtos")
+            // Consulte o documento correspondente ao produto no Firestore com base no nome do produto
+            val querySnapshot = produtosCollection
+                .whereEqualTo("nome", produto.nome)
+                .get()
+                .await()
+
+            for (document in querySnapshot) {
+                val documentId = document.id
+                // Atualize o campo "marcado" no documento correspondente
+                produtosCollection.document(documentId)
+                    .update("marcado", produto.marcado)
+                    .await()
+
+                Log.d("MainActivity", "Produto atualizado no Firebase: $produto")
             }
         }
         override fun getItemCount(): Int {//trazer o tamanho da list
             return dataSet.size
         }
-
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val db = FirebaseFirestore.getInstance()
         super.onActivityResult(requestCode, resultCode, data)
         //if para recuperar os dados e criar
         if (requestCode == SEU_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
@@ -147,27 +194,28 @@ class MainActivity : AppCompatActivity() {
             val quantidadep = data?.getIntExtra("qtd_produto", 0)
 
             if (nome != null && nome.isNotEmpty() && quantidadep != null) {
-                var contadorProdutos = sharedPreferences.getInt("contador_produtos", 0)
-                //cadastro
-                contadorProdutos++
-                editor.putInt("contador_produtos", contadorProdutos)
-                editor.putString("produto_${contadorProdutos}_id", contadorProdutos.toString())
-                editor.putString("produto_${contadorProdutos}_nome", nome)
-                editor.putInt("produto_${contadorProdutos}_quantidade", quantidadep)
-                editor.putBoolean("produto_${contadorProdutos}_marcado", false)
-                editor.apply()
-                //cadastro na list
-                val novoProduto = Produto(contadorProdutos.toString() ,nome, quantidadep, false)
+                val novoProduto = Produto(nome, quantidadep, false)
+
+                db.collection("Produtos") // Substitua "produtos" pelo nome da sua coleção
+                    .add(novoProduto)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(
+                            "MainActivity",
+                            "Produto adicionado com o ID: ${documentReference.id}"
+                        )
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("MainActivity", "Erro ao adicionar Produto", e)
+                    }
                 dataset.add(novoProduto)
                 customAdapter.notifyItemInserted(dataset.size - 1)
                 val duracaoCurta = Toast.LENGTH_LONG
                 Toast.makeText(this, "Adicionado", duracaoCurta).show()
 
-                Log.d("MainActivity", "Novo Produto Criado - Nome: $nome, Quantidade: $quantidadep, id: $contadorProdutos")
-            }else{
+                Log.d("MainActivity", "Novo Produto Criado - Nome: $nome, Quantidade: $quantidadep")
+            } else {
                 val duracaoCurta = Toast.LENGTH_LONG
                 Toast.makeText(this, "Dados Incompletos", duracaoCurta).show()
-
                 Log.d("MainActivity", "Dados da Activity não recebidos corretamente")
             }
         }
@@ -175,26 +223,61 @@ class MainActivity : AppCompatActivity() {
 
     fun PaginaAdicionarProduto(view: View) {
         val intent = Intent(this, ActivityAdicionar::class.java)
-        startActivityForResult(intent, SEU_REQUEST_CODE)//chama uma nova activity e espera um resultado
-
+        startActivityForResult(
+            intent,
+            SEU_REQUEST_CODE
+        )//chama uma nova activity e espera um resultado
     }
 
-    fun LimparProduto(view: View)
-    {
-        val iterator = dataset.iterator()
-        while (iterator.hasNext()) {//percorro a list
-            val produto = iterator.next()
+    fun LimparProduto(view: View) {
+        val produtosParaExcluir = mutableListOf<Produto>()
+        for (produto in dataset) {
             if (produto.marcado) {
-                iterator.remove() // Remove produtos marcados da lista e depois do sharedPreference
-                editor.remove("produto_${produto.id}_id")
-                editor.remove("produto_${produto.id}_nome")
-                editor.remove("produto_${produto.id}_quantidade")
-                editor.remove("produto_${produto.id}_marcado")
-                editor.apply()
+                produtosParaExcluir.add(produto)
             }
         }
-        customAdapter.notifyDataSetChanged()
-        val duracaoCurta = Toast.LENGTH_LONG
-        Toast.makeText(this, "Removido com Sucesso", duracaoCurta).show()
+        CoroutineScope(Dispatchers.Main).launch {
+            val sucesso = excluirProdutosMarcadosNoFirebase(produtosParaExcluir)
+            if (sucesso) {
+                // Remova os produtos da lista após a exclusão
+                dataset.removeAll(produtosParaExcluir)
+                customAdapter.notifyDataSetChanged()
+
+                val duracaoCurta = Toast.LENGTH_LONG
+                Toast.makeText(this@MainActivity, "Produtos excluídos com sucesso", duracaoCurta)
+                    .show()
+            } else {
+                val duracaoCurta = Toast.LENGTH_LONG
+                Toast.makeText(this@MainActivity, "Falha ao excluir produtos", duracaoCurta).show()
+            }
+        }
+    }
+    private suspend fun excluirProdutosMarcadosNoFirebase(produtosParaExcluir: List<Produto>): Boolean {
+        return withContext(Dispatchers.IO) {
+            val db = FirebaseFirestore.getInstance()
+            val produtosCollection = db.collection("Produtos")
+            try {
+                for (produto in produtosParaExcluir) {
+                    val querySnapshot = produtosCollection
+                        .whereEqualTo("nome", produto.nome)
+                        .get()
+                        .await()
+
+                    for (document in querySnapshot) {
+                        val documentId = document.id
+
+                        // Exclua o documento correspondente
+                        produtosCollection.document(documentId)
+                            .delete()
+                            .await()
+                        Log.d("MainActivity", "Produto excluído do Firebase: ${produto.nome}")
+                    }
+                }
+                return@withContext true
+            } catch (e: Exception) {
+                Log.w("MainActivity", "Erro ao excluir produtos do Firebase", e)
+                return@withContext false
+            }
+        }
     }
 }
